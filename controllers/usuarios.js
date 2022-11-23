1658d215e3995556a36e70787dc6e0f9c2e439b8
OlafGG <baezauriel01@gmail.com>
2022-11-22 18:13:50 -0600

    Regex para correo electronico

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,7 @@
 const Usuario = require('../models/Usuario');
 
+
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 const getUsuarios = async (req, res) => {
     // const query = await db.query("SELECT * FROM usuarios");
     const query = await Usuario.findAll();
@@ -20,14 +22,20 @@ const postUsuario = async (req, res) => {
         const data = req.body;
         if (data.nombreUsuario && data.password && data.apellidosUsuario && data.correoElectronico){
             console.log(data);
+            if(data.correoElectronico.match(mailformat)){
+                const usuario = await Usuario.create(data);
 
-        const usuario = await Usuario.create(data);
-
-        return res.status(200).json({
-            ok: true,
-            message: "Usuario almacenado correctamente",
-            data: usuario
-        });
+                return res.status(200).json({
+                    ok: true,
+                    message: "Usuario almacenado correctamente",
+                    data: usuario
+                });
+            }else{
+                return res.status(200).json({
+                    ok: false,
+                    message: 'Correo electronico mal escrito'
+                });
+            }
         }
 
         return res.status(500).json({
