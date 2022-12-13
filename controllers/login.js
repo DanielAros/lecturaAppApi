b618cb8d2655820d9285a3270608d99fb831b870
Daniel Aros Ramírez <danielaros200@gmail.com>
2022-12-13 12:50:41 -0600

    Se corrigieron los errores de estado de respuesta

--- controllers/login.js
+++ controllers/login.js
@@ -7,13 +7,13 @@ const postLogin = async (req, res) => {
     const usuario = await Usuario.findOne({where: {nombreUsuario}});
 
     if(!usuario){
-        return res.status(404).json({ok: false, code: 200, message: "Usuario no encontrado"});
+        return res.status(200).json({ok: false, code: 200, message: "Usuario no encontrado"});
     }else{
         const checkPassword = await compare(password, usuario.password);
         if(checkPassword){
             return res.status(200).json({ok: true, code: 200, message: usuario});
         }else{
-            return res.status(501).json({ok: false, code: 501, message: "Constraseña equivocada"});
+            return res.status(200).json({ok: false, code: 501, message: "Constraseña equivocada"});
         }
     }
     
